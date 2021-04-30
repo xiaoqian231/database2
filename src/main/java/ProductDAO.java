f859a7a2cf732cd2dda4b48d7f7767d7d2af070d
xiaoqian <xiaoqian.gao1@gmail.com>
2021-04-30 17:54:43 +0200

    change DAO

--- src/main/java/ProductDAO.java
+++ src/main/java/ProductDAO.java
@@ -79,7 +79,7 @@ public class ProductDAO {
             while (rs.next())
                 System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4));
             con.close();
-
+            System.out.println(123);
 
         }
 
